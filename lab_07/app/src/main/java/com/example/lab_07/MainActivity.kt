a4ed4b417a319eb4e6bd98c5c03196a7a0b44ec1
Reid <reidpritchard99@gmail.com>
2021-11-11 09:54:17 -0700

    Added clean lab 7

--- lab_07/app/src/main/java/com/example/lab_07/MainActivity.kt
+++ lab_07/app/src/main/java/com/example/lab_07/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lab_07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.*
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var coolMusic: CheckBox
     lateinit var coolResume: CheckBox
     lateinit var coolButton: Button
+    lateinit var scoreView: TextView
+    var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,18 +33,58 @@ class MainActivity : AppCompatActivity() {
         coolResume = findViewById(R.id.coolResume)
 
         coolButton = findViewById(R.id.checkCoolButton)
-        coolButton.setOnClickListener(coolCheck())
+        coolButton.setOnClickListener {
+           coolCheck(it)
+        }
+
+        scoreView = findViewById(R.id.scoreViewer)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("score", score)
+    }
 
-    private fun coolCheck(): View.OnClickListener? {
-        var score = 0
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        score = savedInstanceState.getInt("score")
+        scoreView.text = score.toString()
+    }
 
-        if (coolSwitch.isActivated) score + 10
-        Log.d("score", score.toString())
+    private fun coolCheck(v: View) {
+        score = 0
 
-        var radioTemp = coolGroup.checkedRadioButtonId()
-//        if(coolGroup.getChildAt(radioTemp).getText())
+        if (coolSwitch.isChecked) score += 10
+//        Log.d("score", score.toString())
 
+        // Get radio button
+        val idx = coolGroup.checkedRadioButtonId
+        if (idx != -1) {
+            val radioTemp: View = coolGroup.findViewById(idx)
+            val index = coolGroup.indexOfChild(radioTemp)
+            val selected: RadioButton = coolGroup.getChildAt(index) as RadioButton
+
+            // Check radio button selection
+            if (selected.text.toString() == resources.getString(R.string.Cool)) score += 2
+            if (selected.text.toString() == resources.getString(R.string.Cooool)) score += 6
+            if (selected.text.toString() == resources.getString(R.string.Cooooool)) score += 20
+        } else {
+            score -= 10
+        }
+
+        score += coolCrank.progress * 10
+
+        // Get spinner values and selection
+        val spinnerPos = coolSpinner.selectedItemPosition
+        val spinnerValues = resources.getStringArray(R.array.coolestPickValues)
+
+        score += spinnerValues[spinnerPos].toInt()
+
+        // Check boxes
+        if (coolArtist.isChecked) score += 10
+        if (coolMusic.isChecked) score += 5
+        if (coolResume.isChecked) score += 100
+
+        scoreView.text = score.toString()
     }
 }
